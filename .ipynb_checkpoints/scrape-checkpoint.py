238aa074ac7d00639970571cd7f28104a9b8ca6b
Siddharth Saraf <saraf.siddharth@gmail.com>
2025-04-21 19:55:27 -0700

    Added a little for getting urls off arxiv ai papers

---
+++ .ipynb_checkpoints/scrape-checkpoint.py
@@ -0,0 +1,13 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import re
+import time
+from datetime import datetime
+import os
+
+def main():
+    
+
+if __name__ == "__main__":
+    main()
